2979201f4c9e985d22abf463317295755ca9dc79
Léonce Aklin <leonce.aklin@zebrapig.com>
2023-05-22 16:50:07 +0200

    Route fix

--- pages/[...catchall].vue
+++ pages/[...catchall].vue
@@ -1,14 +1,12 @@
 <template>
     <PlasmicRootProvider :loader="PLASMIC" :prefetchedData="plasmicData">
-        <PlasmicComponent :component="$route.fullPath" />
+        <PlasmicComponent :component="route.fullPath" />
     </PlasmicRootProvider>
 </template>
 
 <script setup lang="ts">
-import pkg from "@plasmicapp/loader-vue";
+import { PlasmicRootProvider, PlasmicComponent }  from "@plasmicapp/loader-vue";
 import { PLASMIC } from "../plasmic-init";
-
-const {  PlasmicRootProvider, PlasmicComponent } = pkg
 
 const route = useRoute();
 const plasmicData = await PLASMIC.fetchComponentData(route.fullPath);
