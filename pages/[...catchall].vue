816ee598647dd195cb95699824abad4d79afd14e
Léonce Aklin <leonce.aklin@zebrapig.com>
2023-05-22 16:34:03 +0200

    Plasmic test

---
+++ pages/[...catchall].vue
@@ -0,0 +1,13 @@
+<template>
+    <PlasmicRootProvider :loader="PLASMIC" :prefetchedData="plasmicData">
+        <PlasmicComponent :component="$route.fullPath" />
+    </PlasmicRootProvider>
+</template>
+
+<script setup lang="ts">
+import { PlasmicRootProvider, PlasmicComponent } from "@plasmicapp/loader-vue";
+import { PLASMIC } from "../plasmic-init";
+
+const route = useRoute();
+const plasmicData = await PLASMIC.fetchComponentData(route.fullPath);
+</script>
