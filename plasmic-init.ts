172ceda97a3fdd6c762c8b03cc8625644faa26a1
Léonce Aklin <leonce.aklin@zebrapig.com>
2023-05-29 15:55:48 +0200

    Plasmic integrated

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,4 +1,5 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-vue";
+
 export const PLASMIC = initPlasmicLoader({
     projects: [
         {
@@ -6,7 +7,4 @@ export const PLASMIC = initPlasmicLoader({
             token: "xCIjIM9pye0hSThHaECnitxv2FTB31W6g68GwFWQxQ0tE4lZ5mf2fcLYXiMiEIXZxP4dW5K3gKVkKNtimGuw"  // API token for that project
         }
     ],
-    // Fetches the latest revisions, whether or not they were unpublished!
-    // Disable for production to ensure you render only published changes.
-    preview: true,
 })
